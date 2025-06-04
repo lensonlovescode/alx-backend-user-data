9739c0455c7ffa6aabadcffd7566e28091cdeec1
lensonlovescode <lensonisinparis@gmail.com>
2025-06-04 11:24:19 +0300

    base64 extraction

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -9,3 +9,16 @@ class BasicAuth(Auth):
     """
     The basic authentication class that inherits from the auth class
     """
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """
+        Extracts the base64 authorization header
+        """
+        if authorization_header is None:
+            return None
+
+        arr = str(authorization_header).split(' ')
+        if arr[0] != "Basic":
+            return None
+
+        return arr[1]
